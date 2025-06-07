438dc07cb0e671d06cc5b66b8c54bf148da02c93
Marie-Anne Grand <marie-annegrand@Marie-Annes-MacBook-Pro.local>
2025-06-07 13:08:03 +0200

    Add Icons

---
+++ src/components/Icon/NoSearchFound.tsx
@@ -0,0 +1,36 @@
+const NoSearchFound = () => {
+  return (
+    <svg
+      fill="none"
+      height="52"
+      viewBox="0 -0.5 25 25"
+      width="52"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        clipRule="evenodd"
+        d="M5.5 11.493a6.5 6.5 0 1 1 13 .015 6.5 6.5 0 0 1-13-.015"
+        stroke="#000"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+      >
+      </path>
+      <path
+        d="m16.062 16.568 3.438 3.425"
+        stroke="#000"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+      >
+      </path>
+      <path
+        d="M10.53 8.963a.75.75 0 1 0-1.06 1.06zm.94 3.06a.75.75 0 1 0 1.06-1.06zm1.06-1.06a.75.75 0 1 0-1.06 1.06zm.94 3.06a.75.75 0 1 0 1.06-1.06zm-.94-2a.75.75 0 0 0-1.06-1.06zm-3.06.94a.75.75 0 0 0 1.06 1.06zm2-2a.75.75 0 0 0 1.06 1.06zm3.06-.94a.75.75 0 0 0-1.06-1.06zm-5.06 0 2 2 1.06-1.06-2-2zm2 2 2 2 1.06-1.06-2-2zm0-1.06-2 2 1.06 1.06 2-2zm1.06 1.06 2-2-1.06-1.06-2 2z"
+        fill="#000"
+      >
+      </path>
+    </svg>
+  );
+};
+
+export default NoSearchFound;
